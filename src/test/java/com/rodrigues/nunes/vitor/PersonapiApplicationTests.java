bb9d5d486dbb3a06529d24a6ccd14da82efe7b8d
Vitor <vitorrodrigues0201@gmail.com>
2021-10-15 21:25:13 -0300

    First Commit

---
+++ src/test/java/com/rodrigues/nunes/vitor/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigues.nunes.vitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
